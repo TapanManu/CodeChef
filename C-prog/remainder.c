d1a0ab4ee5afc757abcffc18a30215e5b576de03
TapanManu <48797926+TapanManu@users.noreply.github.com>
2019-12-07 17:47:36 +0530

    Update remainder.c

--- C-prog/remainder.c
+++ C-prog/remainder.c
@@ -1,1 +1,15 @@
+#include <stdio.h> 
 
+int main() {
+	// Read the number of test cases.
+	int T,a,b;
+	scanf("%d", &T);
+	while (T--) {
+		scanf("%d %d",&a,&b);
+		if(b!=0)
+		{
+        printf("%d\n",a%b);
+		}
+	}
+	return 0;
+}
